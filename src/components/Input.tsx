11d30e0d0c916fb25bf4cf1a60b4ef19a8707c4d
Ondrej Horovsky <ondrej.h@day8.com>
2020-04-22 15:18:51 +0200

    useRef types

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 
 export const Input = () => {
     const [name, setName] = useState('')
     // const [name, setName] = useState<string | null>(null)
-    return <input value={name} onChange={(e) => setName(e.target.value)} />
+    // !null Read Only
+    const ref = useRef<HTMLInputElement | null>(null!);
+    if(ref && ref.current){
+        console.log('ref', ref.current.value)
+    }
+    return <input ref={ref} value={name} onChange={(e) => setName(e.target.value)} />
 }
