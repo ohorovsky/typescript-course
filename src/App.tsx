9d93c78b697d1b894523dea1b3caa69d3e8119de
Ondrej Horovsky <ondrej.h@day8.com>
2020-04-22 17:18:10 +0200

    added more context stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Head } from "./components/Head";
 import Button from "./components/Button";
 import { Input } from "./components/Input";
-import { GlobalContext, initialValues } from "./components/GlobalState";
+import { GlobalProvider } from "./components/GlobalState";
 import { ReducerButtons } from "./components/ReducerButtons";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <GlobalContext.Provider value={initialValues}>
+    <GlobalProvider>
       <div className="App">
         <Head title="Meg" />
         <header className="App-header">
@@ -37,7 +37,7 @@ function App() {
           </a>
         </header>
       </div>
-    </GlobalContext.Provider>
+    </GlobalProvider>
   );
 }
 
--- src/components/ReducerButtons.tsx
+++ src/components/ReducerButtons.tsx
@@ -2,42 +2,18 @@ import React, { useReducer, useRef, useContext } from "react";
 import { useClickOutside } from "./useClickOutside";
 import { GlobalContext } from "./GlobalState";
 
-const initialState = { rValue: true };
-
-type State = {
-  rValue: boolean;
-};
-
 // type Action = { type: "one" } | { type: "two" };
-type Action =
-  | { type: "one" }
-  | { type: "two" }
-  | { type: "three" }
-  | { type: "four" };
-
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case "one":
-      return { rValue: true };
-    case "two":
-      return { rValue: false };
-    default:
-      return state;
-  }
-}
 
 export const ReducerButtons = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
   const ref = useRef<HTMLDivElement>(null!);
-  const { rValue } = useContext(GlobalContext);
+  const { rValue, turnOn, turnOff } = useContext(GlobalContext);
   useClickOutside(ref, () => {
     console.log("clicked outside");
   });
   return (
     <div ref={ref}>
-      <button onClick={() => dispatch({ type: "one" })}>Action 1</button>
-      <button onClick={() => dispatch({ type: "two" })}>Action 2</button>
-      <button onClick={() => dispatch({ type: "three" })}>Action 3 </button>
+      <button onClick={turnOn}>Action 1</button>
+      <button onClick={turnOff}>Action 2</button>
       {rValue && <p>visible</p>}
     </div>
   );
