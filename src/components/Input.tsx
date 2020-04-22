70f2a415f6aee187d93a208c0da562f700e32cd4
Ondrej Horovsky <ondrej.h@day8.com>
2020-04-22 15:31:12 +0200

    optional null chain

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,9 +4,7 @@ export const Input = () => {
     const [name, setName] = useState('')
     // const [name, setName] = useState<string | null>(null)
     // !null Read Only
-    const ref = useRef<HTMLInputElement | null>(null!);
-    if(ref && ref.current){
-        console.log('ref', ref.current.value)
-    }
+    const ref = useRef<HTMLInputElement>(null);
+    console.log('ref', ref?.current?.value)
     return <input ref={ref} value={name} onChange={(e) => setName(e.target.value)} />
 }
